5ed57f204c469930cfe57258a9240c199c133f5b
Frans Allen <fransallen@users.noreply.github.com>
2025-12-21 20:19:46 +0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,28 @@ const nextConfig = {
     unoptimized: true,
   },
   devIndicators: false,
+  async headers() {
+    return [
+      {
+        source: "/images/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/favicon.ico",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
